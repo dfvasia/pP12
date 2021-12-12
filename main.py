7d6f1993fef2ec30bf320cd6a90f1b22b6f93f39
dfvasia <vasilii-zhuravsk@mail.ru>
2021-12-12 23:44:17 +0300

    add  middle work

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from funtions import open_file, person_data
 from models import Person
 
@@ -22,26 +22,53 @@ def is_online():
 @app.route('/candidate/<int:id_x>')
 def candidate(id_x):
     while True:
-        if 0 < id_x <= 6:
+        if 0 < id_x <= 7:
             idx = id_x -1
             id_x_t = id_x + 1
-            print(person_data_user[idx].name)
             person_data_user_name = person_data_user[idx].name
             person_data_user_position = person_data_user[idx].position
             person_data_user_picture = person_data_user[idx].picture_url
             person_data_user_skills = person_data_user[idx].skills
-            return render_template("p_candidate.html", person_data_user_name=person_data_user_name,
+            if id_x == 7:
+                id_x_t = 1
+                return render_template("p_candidate.html", person_data_user_name=person_data_user_name,
+                                   person_data_user_position=person_data_user_position,
+                                   person_data_user_picture=person_data_user_picture,
+                                   person_data_user_skills=person_data_user_skills, id_x_t=id_x_t)
+            else:
+                return render_template("p_candidate.html", person_data_user_name=person_data_user_name,
                                    person_data_user_position=person_data_user_position,
                                    person_data_user_picture=person_data_user_picture,
                                    person_data_user_skills=person_data_user_skills, id_x_t=id_x_t)
 
-    else:
-        return redirect("http://127.0.0.1:5000/candidate/1", code=302)
+        else:
+            return redirect("http://127.0.0.1:5000/candidate/1", code=302)
 
 
 @app.route('/list/')
 def list():
-    pass
-#     return "<h1>Имя кандидата</h1> <p>Позиция кандидата</p> <img src={{картинка}} width=200/> <p>Навыки кандидата</p>"
+    list_person = person_data_user
+    return render_template("list.html", list_person=list_person)
+
+@app.route('/search/')
+def p_search():
+    p_search = request.args.get("name")
+    # if settings_app["case-sensitive"] is False:
+    #     p_search.lower()
+    list_person = person_data_user
+    temp_search = {}
+    if p_search:
+        if settings_app["case-sensitive"] is False:
+            p_s = p_search.lower()
+            for person in list_person:
+                if p_s in person.name.lower():
+                    temp_search[person.id_p] = person.name
+        else:
+            for person in list_person:
+                if p_search in person.name:
+                    temp_search[person.id_p] = person.name
+
+    return render_template("search.html", p_search=temp_search, temp_search=temp_search)
+
 
 app.run(debug=True)
